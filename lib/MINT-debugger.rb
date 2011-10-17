7a6bd53ae68d5bddbbed3add177f429c00aeee75
Sebastian Feuerstack <Sebastian@Feuerstack.de>
2011-10-17 17:01:17 -0200

    - fixed locatioon

--- lib/MINT-debugger.rb
+++ lib/MINT-debugger.rb
@@ -1,4 +1,5 @@
- $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module MINTDebugger
   VERSION = '0.0.1'
